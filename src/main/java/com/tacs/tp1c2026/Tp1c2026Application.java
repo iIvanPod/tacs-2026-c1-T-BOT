999d1d59974f4945bf17bf2cdabd00810f5365ca
Ivan Preinsperger <ipreinsperger@frba.utn.edu.ar>
2026-04-28 00:54:24 -0300

    Agregando spring

---
+++ src/main/java/com/tacs/tp1c2026/Tp1c2026Application.java
@@ -0,0 +1,13 @@
+package com.tacs.tp1c2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp1c2026Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp1c2026Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/tacs/tp1c2026/Tp1c2026ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tacs.tp1c2026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp1c2026ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
